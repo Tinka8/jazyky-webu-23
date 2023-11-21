cc9d11ccd355033009761312b7a712ccd100772f
Tinka8 <lenmatka818@gmail.com>
2023-11-21 15:53:11 +0100

    responsive app

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-cislo = int(input())
-delitele = []
-
-for i in range(1, cislo + 1):
-  if cislo % i == 0:
-    delitele.append(i)
-  else:
-    i += 1
-
-delitele.sort(reverse=True)
-map(str, delitele)
-print(" ,".join(delitele))
+from test import prvocislo
+  
+print(prvocislo(int(input())))
